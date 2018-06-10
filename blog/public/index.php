609e65ffea69d929809eac511ee1ba5ea259dd6e
Fernando Torres <fertorresmx@gmail.com>
2018-06-10 00:39:18 -0500

    Archivos Video 42 'Introducción a Router'

--- blog/public/index.php
+++ blog/public/index.php
@@ -4,18 +4,31 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+    require_once '../vendor/autoload.php';
+
     include_once '../config.php';
 
     $route = $_GET['route'] ?? '/';
 
-    switch ($route) {
-        case '/':
-            require '../index.php';
-            break;
-        case '/admin':
-            require '../admin/index.php';
-            break;
-        case '/admin/posts':
-            require '../admin/posts.php';
-            break;
-    }
+    use Phroute\Phroute\RouteCollector;
+
+    $router = new RouteCollector();
+    $router->get('/', function() use ($pdo) {
+
+        $query = $pdo->prepare('SELECT * FROM blog_posts ORDER BY id DESC');
+        $query->execute();
+
+        $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
+        include '../views/index.php';
+    });
+
+    $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+    $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+    echo $response;
+
+
+//    $router->get($route, $handler);    # match only get requests
+//    $router->post($route, $handler);   # match only post requests
+//    $router->delete($route, $handler); # match only delete requests
+//    $router->any($route, $handler);    # match any request method
+
