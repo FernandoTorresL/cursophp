ba04a3a839bee2e26e0e9da835ee9ab20c45434f
Fernando Torres <fertorresmx@gmail.com>
2018-06-28 00:10:48 -0500

    Archivos video 14 'Funciones en PHP'

---
+++ functions/index.php
@@ -0,0 +1,23 @@
+<?php
+
+//function hello($name) {
+//    echo 'Hello ' . $name;
+//    echo '<br>';
+//}
+//
+//hello('Mary');
+//hello('Elizabeth');
+
+$x = 100;
+
+function sum($a, $b) {
+    $x = $a + $b;
+    return $x;
+}
+
+$c = sum(5, 6);
+var_dump($c);
+
+$c = sum(11, 22);
+var_dump($c);
+var_dump($x);
