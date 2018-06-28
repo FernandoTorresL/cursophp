1c31963a68ec38d36dc219fc8c00aa7013985e63
Fernando Torres <fertorresmx@gmail.com>
2018-06-28 11:43:08 -0500

    Archivos video 27 'Interface'

--- oop/index.php
+++ oop/index.php
@@ -7,6 +7,10 @@ include 'Vehicles/Truck.php';
 //use Vehicles\Truck;
 use Vehicles\{Car, Truck};
 
+//interface MyInterface {
+//    public function method1();
+//}
+
 echo 'Class Car<br>';
 $car = new Car('Alex');
 $car->move();
@@ -24,3 +28,15 @@ echo '<br>Total Trucks: ' . Truck::getTotal() . '<br><br>';
 
 //$v1 = new \Vehicles\VehicleBase('Alex');
 //$v1->move();
+
+echo 'var_dump($car): ';
+var_dump($car);
+echo '<br>';
+$ser = serialize($car);
+
+echo 'var_dump(ser): ';
+var_dump($ser);
+echo '<br>';
+$newCar = unserialize($ser);
+
+echo 'NewCar Owner: ' . $newCar->getOwner() . '<br>';
